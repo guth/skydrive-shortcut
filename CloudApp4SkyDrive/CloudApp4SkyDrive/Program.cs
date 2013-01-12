956ce9390428b9c1fcc04f0eec6a08550654c4b1
Brendan Wright <bmwtwo@princeton.edu>
2013-01-11 16:19:18 -0800

    Initial commit. Not very functional.

---
+++ CloudApp4SkyDrive/CloudApp4SkyDrive/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CloudApp4SkyDrive
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using (TrayIcon ti = new TrayIcon())
+            {
+                //ti.Display();
+                Application.Run();
+            }
+        }
+    }
+}
---
+++ CloudApp4SkyDrive/CloudApp4SkyDrive/TrayIcon.cs
@@ -0,0 +1,20 @@
+﻿// TODO: some imports probably not necessary
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CloudApp4SkyDrive
+{
+    class TrayIcon : IDisposable
+    {
+        NotifyIcon ni;
+
+        public TrayIcon()
+        {
+            ni = new NotifyIcon();
+        }
+    }
+}
